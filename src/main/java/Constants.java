e6cb35beeab2fa34c8d9c0fdcd06d88f5476c448
Olha Yeremenko <Olha_Yeremenko@epam.com>
2015-10-13 20:21:25 +0300

    Initial commit

---
+++ src/main/java/Constants.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Olha_Yeremenko on 13.10.2015.
+ */
+public class Constants {
+}
---
+++ src/main/java/Demo.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Olha_Yeremenko on 13.10.2015.
+ */
+public class Demo {
+
+    public static void main(String[] args) throws Throwable {
+//        downloadFileFromURL("ttps://dl.dropboxusercontent.com/u/98396761/NewTxt.txt",  new File("c:\\file1.txt"));
+        System.out.println("Hello word!");
+    }
+
+
+//    public static void downloadFileFromURL(String urlString, File destination) throws IOException {
+//
+//        URL website = new URL(urlString);
+//        try(
+//                ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+//                FileOutputStream fos = new FileOutputStream(destination);
+//        ){
+//            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+//        }
+//
+//    }
+}
