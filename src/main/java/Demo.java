502388444b7bec94739283ce4465a3334d1252ce
Olha Yeremenko <Olha_Yeremenko@epam.com>
2015-10-13 20:48:59 +0300

    Demo class updated

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,23 +1,21 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
 /**
  * Created by Olha_Yeremenko on 13.10.2015.
  */
 public class Demo {
 
-    public static void main(String[] args) throws Throwable {
-//        downloadFileFromURL("ttps://dl.dropboxusercontent.com/u/98396761/NewTxt.txt",  new File("c:\\file1.txt"));
-        System.out.println("Hello word!");
+
+    public static void main(String args[]) throws IOException {
+
+        URL website = new URL("https://dl.dropboxusercontent.com/u/98396761/NewTxt.txt");
+        ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+        FileOutputStream fos = new FileOutputStream("File.txt");
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        System.out.println("File created");
     }
-
-
-//    public static void downloadFileFromURL(String urlString, File destination) throws IOException {
-//
-//        URL website = new URL(urlString);
-//        try(
-//                ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-//                FileOutputStream fos = new FileOutputStream(destination);
-//        ){
-//            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-//        }
-//
-//    }
 }
